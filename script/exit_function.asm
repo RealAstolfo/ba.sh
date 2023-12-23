5086a50bf055b1a2c7894d82c44850c8365db818
Astolfo <astolfo@gmx.com>
2023-12-22 19:54:18 -0700

    added exit_function.asm
    added next challenge where register "dil" is set, which actually overlaps with register rdi, and therefore
    the values should be interlinked somewhat.

---
+++ script/exit_function.asm
@@ -0,0 +1,15 @@
+%include "syscalls.asm"	; requires syscall listing for your OS in lib/sys/	
+
+%include "lib/sys/exit.asm"	
+; void exit(byte {dil});
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;INSTRUCTIONS;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+START:
+
+	mov dil,69		; {rdi} = funny number
+	call exit
+
+END:
